6823c62e930fcbce5df19f091523d13cae8fce86
Dawayne Eason <37123932+tazexpress24@users.noreply.github.com>
2018-07-31 17:50:23 -0400

    updated

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "vku13m79yH7lj9heRQKldCbGk"
+consumer_secret = "y9ZBCDlbBq5CpWfx7A7xvQSSBg7m4BcYHz534HSKPDsGpxZw9A"
+access_token = "1016433964096073729-YH4qAdbpIhXEeusgJSxG15CkpZa1at"
+access_token_secret = "LR04wDEOZK7hIQY2z9KWdoMvYtFOOL0uMezwh7Gx4EBXR"
